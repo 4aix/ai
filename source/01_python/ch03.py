3aeb2863142f0fb066f22a76dd584d4a35c6fef8
4aix <comlec0703@gmail.com>
2026-06-26 12:51:33 +0900

    데이터구조

---
+++ source/01_python/ch03.py
@@ -0,0 +1,8 @@
+def safe_index(lst, item):
+    '''
+    첫번째 매개변수 lst에서 item 요소가 있는 index를 반환하는 함수
+    item 요소가 없으면 -1를 반환
+    '''
+    if item in lst:
+        return lst.index(item)
+    return -1
