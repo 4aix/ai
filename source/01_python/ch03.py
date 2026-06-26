27cac4ae87a0734d1de06673541294b809e206bf
4aix <comlec0703@gmail.com>
2026-06-26 17:22:22 +0900

    데이터구조

--- source/01_python/ch03.py
+++ source/01_python/ch03.py
@@ -6,3 +6,14 @@ def safe_index(lst, item):
     if item in lst:
         return lst.index(item)
     return -1
+
+
+def safe_remove(lst, remove_item):
+    '''
+    첫번째 매개변수 lst에서 remove_item 데이터 제거
+    해당 remove_item이 lst에 없으면 메세지 출력
+    '''
+    if remove_item in lst:
+        lst.remove(remove_item)
+    else:
+        print('해당 데이터는 없는 데이터입니다')
