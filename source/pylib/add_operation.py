5b870adb134b2fbc69589685afdbc8794db31616
4aix <comlec0703@gmail.com>
2026-07-06 11:48:14 +0900

    모듈과패키지

--- source/pylib/add_operation.py
+++ source/pylib/add_operation.py
@@ -5,6 +5,13 @@ def add_print(n):
         tot += i # tot = tot+i
     print(f"1부터 {n}까지 누적합은 {tot}입니다")
 
+def add_return(n):
+    "1부터 n까지 누적합을 return"
+    tot = 0
+    for i in range(1, n+1):
+        tot += i # tot = tot+i
+    return tot
+
 
 
 
