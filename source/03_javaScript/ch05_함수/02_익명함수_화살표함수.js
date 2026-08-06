5637af3092b841fe131c8a472e3ab778c9454733
4aix <comlec0703@gmail.com>
2026-08-06 10:36:47 +0900

    JS함수

--- source/03_javaScript/ch05_함수/02_익명함수_화살표함수.js
+++ source/03_javaScript/ch05_함수/02_익명함수_화살표함수.js
@@ -6,3 +6,8 @@ funVar = () => {
   console.log('2. 매개변수가 없거나 2개이상인 화살표함수 호출');
 };
 funVar();
+funvar = i => {
+  console.log('3. 매개변수가 하나 있는 화살표 함수 호출');
+  console.log('매개변수 i =', i);
+};
+funvar();
