4185f3bd11588c8f9f5a88aae7fae90473cbe19d
4aix <comlec0703@gmail.com>
2026-08-06 12:13:46 +0900

    JS함수

--- source/03_javaScript/ch05_함수/07_array.js
+++ source/03_javaScript/ch05_함수/07_array.js
@@ -1,9 +1,20 @@
 /* array함수 : 가변인자함수(화살표함수는 불가)
- * 매개변가 0개 :
- * 매개변수 1개 :
- * 매개변수 2개 이상 : */
-function array(){
-  console.log()
+ * 매개변가 0개 : length가 0인 배열을 return
+ * 매개변수 1개 : length가 매개변수만큼인 크기의 배열을 return
+ * 매개변수 2개 이상 : 매개변수로 배열을 생성 return */
+function array(){ // arguments : 매개변수 내용이 들어옴/arguments.length:매개변수갯수
+  //console.log(arguments, arguments.length);
+  let result = [];
+  if(arguments.length==1){
+    // result를 arguments[0] 만큼 크기의 빈 배열
+    for(let cnt=1 ; cnt<=arguments[0]; cnt++){
+      result.push(undefined);
+    }
+  }else if(arguments.length>=2){
+    // result를 arguments의 내용으로 만들기
+
+  }
+  return result;
 }
 array();
 array(3);
