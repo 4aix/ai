c656486bc2f2718ff18ba9c1a3b011b8b7452795
4aix <comlec0703@gmail.com>
2026-08-06 13:05:02 +0900

    JS함수

--- source/03_javaScript/ch05_함수/sumAll.js
+++ source/03_javaScript/ch05_함수/sumAll.js
@@ -1,7 +1,15 @@
 /* 매개변수가 없으면 -999를 리턴하고 매개변수가 1개 이상이면 매개변수들의 누적합을 리턴하는 
   가변인자 함수 sumAll()을 작성한 스크립트 파일(sumAll.js)을 이용하시오. */
 function sumAll(){
-
-}
+  let result = 0;
+  if(arguments.length>0){
+    for(let idx=0 ; idx<arguments.length ; idx++){
+      result += arguments[idx];
+    }//for
+  }else{
+    result = -999;
+  }
+  return result;
+}//sumAll
 console.log(sumAll());
-console.log(sumAll(1, 2, 3));
+console.log(sumAll(1, 2, 3, 4));
