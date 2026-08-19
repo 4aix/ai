34c71f08c6f66091f89a8c1c6cbdf737da84e276
4aix <comlec0703@gmail.com>
2026-08-19 11:22:15 +0900

    Oracle select문

---
+++ source/04_DBMS/0819_02_SELECT문.sql
@@ -0,0 +1,15 @@
+-- [II] SELECT문 - 조회
+
+-- 1. SELECT 문자 작성법(실행:CTRL+ENTER)
+SELECT * FROM TAB; -- 현계정이 가지고 있는 테이블 정보
+SELECT * FROM EMP; -- EMP테이블의 모든 정보(모든 열, 모든행)
+SELECT * FROM DEPT; -- DEPT테이블의 모든 정보
+SELECT * FROM SALGRADE;
+
+-- 2. 특정 열만 출력
+DESC EMP; 
+    -- EMP테이블 구조
+    
+
+
+
