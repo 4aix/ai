1d2823264df2b9a1c25929577c4bbcbe0ebf8fc0
4aix <comlec0703@gmail.com>
2026-08-20 18:00:27 +0900

    join및DDL

--- source/04_DBMS/0821_04_DDL_DML.sql
+++ source/04_DBMS/0821_04_DDL_DML.sql
@@ -16,9 +16,23 @@ CREATE TABLE BOOK(
     BOOKID    NUMBER(4),     -- BOOKID필드의 타입은 숫자4자리
     BOOKNAME  VARCHAR2(30),  -- BOOKNAME필드의 타입은 문자30BYTE(한글1자=3BYTE)
     PUBLISHER VARCHAR2(30),
-    RDATE     DATE,
+    RDATE     DATE,          -- RDATE 필드의 타입은 DATE(날짜+시간)
+    PRICE     NUMBER(8,2),   -- PRICE필드의 타입은 숫자 전체8자리 중 소숫점 2자리
+    PRIMARY KEY(BOOKID)      -- 제약조건 : BOOKID를 주키(PRIMARY KEY)로 
 );
+DESC BOOK;
+SELECT * FROM BOOK;
+DROP TABLE BOOK; -- 2. 테이블 삭제(DROP TABLE 테이블명)
 
+CREATE TABLE BOOK(
+    BOOKID    NUMBER(4) PRIMARY KEY,  -- 제약조건
+    BOOKNAME  VARCHAR2(30),  
+    PUBLISHER VARCHAR2(30),
+    RDATE     DATE,          
+    PRICE     NUMBER(8,2)
+);
+DROP TABLE BOOK;
+-- DEPT와 유사한 DEPT01테이블 / EMP와 유사한 EMP01테이블 생성
 
 
 
