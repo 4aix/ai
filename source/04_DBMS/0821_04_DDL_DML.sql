f6027c4a7dae7a357cbbc84d14a72d52e4dbaf4f
4aix <comlec0703@gmail.com>
2026-08-20 17:21:35 +0900

    ORACLE 조인

--- source/04_DBMS/0821_04_DDL_DML.sql
+++ source/04_DBMS/0821_04_DDL_DML.sql
@@ -3,4 +3,7 @@
 (1) DCL : 사용자 계정 생성(CREATE USER), 권한부여(GRANT), 권한박탈(REVOKE), 사용자 계정 삭제(DROP USER)
           트랜젝션명령어(ROLLBACK, COMMIT)
 (2) DDL : 테이블 생성(CREATE TABLE), 테이블구조변경(ALTER TABLE), 테이블삭제(DROP TABLE)
+(3) DML : CRUD
+      입력(INSERT), 수정(UPDATE), 삭제(DELETE) - 취소(ROLLBACK) 가능
+      검색(SELECT)
 */
