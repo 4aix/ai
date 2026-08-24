564b8a6d37af0c644dd7cf832241b70bdee11194
4aix <comlec0703@gmail.com>
2026-08-24 11:09:59 +0900

    MySQL연동

--- source/04_DBMS/0824_MySQL.sql
+++ source/04_DBMS/0824_MySQL.sql
@@ -1,5 +1,5 @@
 -- DCL(계정생성, 권한부여, 권한박탈, 계정삭제)
--- DDL(타입, 시퀀스없음)
+-- DDL(타입, 시퀀스없음, FK 지정을 아래에)
 -- DML(outer join, and;&&, or;||, 일부함수)
 
 -- ■■■■■■■■■■■■■
@@ -28,6 +28,54 @@ show tables; -- 현재 데이터베이스 내의 테이블들
 실수 : float(n, d ; 4byte), double(n, d;8byte)
 문자 : char(n;비추), text, longtext(4GB)
 */
+drop table if exists friend;
+create table friend(
+	no   int         primary key auto_increment, -- auto_increment 필드타입은 int
+	name varchar(30) not null,
+    tel  varchar(30) unique,
+    age  numeric(3) default 1 check (age>=0),
+    last_modify datetime default now()
+);
+select * from friend;
+insert into friend (name, tel) values ('홍길동', null);
+insert into friend (name, tel, age) values ('성춘향', '010-9999-9999', -2);
+insert into friend (name, tel, age) values ('성춘향', '010-9999-9999', 22);
+
+-- ★ ★ ★  파이썬 수업시간에 쓸 테이블 ★ ★ ★
+-- DDL과 DML 명령어는 데이터베이스안에서만 실행
+select database(); -- 현재 들어와 있는 데이터베이스
+use devdb;
+drop table if exists person; -- emp테이블 유사
+drop table if exists division; -- dept테이블 유사
+create table division(
+	dno int primary key,
+    dname varchar(20), 
+    phone varchar(20),
+    position varchar(20)
+);
+create table person(
+	pno int primary key,
+    pname varchar(15) not null,
+    job   varchar(15) not null,
+    manager int, -- 상사사번
+    hiredate date, -- 입사일
+    sal     numeric(10),
+    comm    numeric(10),
+    dno     int,
+    foreign key(dno) references division(dno) -- FK 제약조건은 반드시 아래에 따로 명시
+);
+select * from division;
+insert into person 
+	values (1111, 'smith','manager', 1001, '1990-12-17', 1000, null, 10);
+select * from person;
+
+
+
+
+
+
+
+
 
 
 
