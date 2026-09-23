6dff7e9e439f35ab0b7d2caa354e04d4812b229e
4aix <comlec0703@gmail.com>
2026-09-23 11:15:09 +0900

    모델저장및flask첫예제

--- source/10_flask/ch1_flaskminiEx/ex2_application.py
+++ source/10_flask/ch1_flaskminiEx/ex2_application.py
@@ -6,3 +6,7 @@ application = Flask(__name__) # 웹어플리케이션 객체 생성
 @application.route('/hello')
 def handler_function():
   return "<h1>Hello, Flask!</h1>"
+
+if __name__=='__main__':
+  # debug=True : 코드가 변경될 때마다 자동 서버 재시작
+  application.run(debug=True, port=80)
